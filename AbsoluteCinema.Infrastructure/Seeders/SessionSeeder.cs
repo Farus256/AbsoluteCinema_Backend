00efe5f2613478be28658aa236c06c254ef50c9e
Oleksandr Velikanov <oleksandr_velikanov@epam.com>
2025-11-05 17:14:06 +0100

    fixed problems, updated

--- AbsoluteCinema.Infrastructure/Seeders/SessionSeeder.cs
+++ AbsoluteCinema.Infrastructure/Seeders/SessionSeeder.cs
@@ -1,29 +1,37 @@
 ﻿using AbsoluteCinema.Domain.Entities;
 using AbsoluteCinema.Infrastructure.DbContexts;
 
-
-namespace AbsoluteCinema.Infrastructure.Seeders
+public class SessionSeeder
 {
-    public class SessionSeeder
+    public static async Task SeedSessionsAsync(AppDbContext context)
     {
-        public static async Task SeedSessionsAsync(AppDbContext context)
+        if (!context.Sessions.Any())
         {
-            if (!context.Sessions.Any())
+            var random = new Random();
+            var halls = context.Halls.ToList();
+            var movies = context.Movies.ToList();
+
+            foreach (var movie in movies)
             {
-                var random = new Random();
-                var randomHall = random.Next(1, context.Halls.Count() + 1);
-                var movies = context.Movies.ToList();
-                foreach (var movie in movies)
+                // Генерация случайного зала
+                var randomHall = halls[random.Next(halls.Count)];
+
+                // Генерация случайной даты и времени: от завтра до 7 дней вперёд
+                var startTime = DateTime.Today.AddDays(random.Next(1, 7))
+                    .AddHours(random.Next(10, 22))     // между 10:00 и 22:00
+                    .AddMinutes(15 * random.Next(0, 4)); // кратно 15 минутам
+
+                var session = new Session
                 {
-                    context.Sessions.Add(new Session() 
-                    { 
-                        MovieId = movie.Id,
-                        HallId = context.Halls.Find(randomHall)!.Id,
-                        Date = (DateTime.Now).AddDays(random.Next(1, 7)),
-                    });
-                }
-                await context.SaveChangesAsync();
+                    MovieId = movie.Id,
+                    HallId = randomHall.Id,
+                    Date = startTime
+                };
+
+                context.Sessions.Add(session);
             }
+
+            await context.SaveChangesAsync();
         }
     }
 }
